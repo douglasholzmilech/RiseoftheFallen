24fc9c620b8576a4dd36afa288b146cc3d1e9ce6
DouglasHM <douglasholzmilech@gmail.com>
2026-05-27 09:45:53 -0300

    Creating the classes and connecting the menu class with the game.

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size=(640, 480))
-print('Setup End')
-
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # close window
-            quit()  # end pygame
+game = Game()
+game.run()
